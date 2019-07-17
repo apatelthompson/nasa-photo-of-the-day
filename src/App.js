ae7791d49cc8ffca1d450c3c06090985d67f2ae8
Avni Patel Thompson <avnipatelthompson@Avnis-iMac.local>
2019-07-16 20:48:33 -0700

    set up project and fetching data from NASA API using developer key

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import React from "react";
-import "./App.css";
+import React, { useState, useEffect } from "react";
+import "./App.scss";
+import axios from "axios";
 
 function App() {
+  const [photo, setPhoto] = useState("");
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.nasa.gov/planetary/apod?api_key=2JAcjwEBaCKS4enUmD0hveRXamLQCVauioOBJ6V2"
+      )
+      .then(response => {
+        console.log(response.data);
+        setPhoto(response.data.message);
+      })
+      .catch(err =>
+        console.log("The NASA API is currently down; please try again later.")
+      );
+  }, []);
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+      <h1>Nasa Photo of The Day</h1>
+      <p>Here are my beautiful photos:</p>
+      <div className="image">{photo}</div>
+      <h3 className="title">{}</h3>
     </div>
   );
 }
